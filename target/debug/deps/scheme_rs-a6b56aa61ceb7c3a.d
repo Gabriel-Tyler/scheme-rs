0744f64727d23f142aa14c5dc31df1c41cf76ae8
Gabe <gabriel.tyler16@gmail.com>
2024-01-11 14:54:25 -0600

    Add ignore

--- target/debug/deps/scheme_rs-a6b56aa61ceb7c3a.d
+++
@@ -1,5 +0,0 @@
-/Users/gabe/dev/rustprojects/scheme_rs/target/debug/deps/libscheme_rs-a6b56aa61ceb7c3a.rmeta: src/main.rs
-
-/Users/gabe/dev/rustprojects/scheme_rs/target/debug/deps/scheme_rs-a6b56aa61ceb7c3a.d: src/main.rs
-
-src/main.rs:
